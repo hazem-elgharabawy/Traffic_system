60d446986a942ce104b21ca39395ee552f41f4cc
HazemElgharabawy <hazem.gharabawy6@gmail.com>
2024-11-28 16:47:55 +0200

    norm_opp_unit DONE

---
+++ norm_opp_unit.v
@@ -0,0 +1,44 @@
+module norm_opp_unit (
+    input [1:0] traffic_light,
+    output reg  allow_0_norm,
+    output reg  allow_1_norm,
+    output reg  allow_2_norm,
+    output reg  allow_3_norm,
+);
+    
+    always @(*) begin
+
+        case (traffic_light)
+            0 : begin
+                allow_0_norm = 1;
+                allow_1_norm = 0;
+                allow_2_norm = 0;
+                allow_3_norm = 0;
+            end
+            1 : begin
+                allow_0_norm = 0;
+                allow_1_norm = 1;
+                allow_2_norm = 0;
+                allow_3_norm = 0;
+            end
+            2 : begin
+                allow_0_norm = 0;
+                allow_1_norm = 0;
+                allow_2_norm = 1;
+                allow_3_norm = 0;
+            end
+            3 : begin
+                allow_0_norm = 0;
+                allow_1_norm = 0;
+                allow_2_norm = 0;
+                allow_3_norm = 1;
+            end 
+            default: begin
+                allow_0_norm = 0;
+                allow_1_norm = 0;
+                allow_2_norm = 0;
+                allow_3_norm = 0;
+            end
+        endcase
+    end
+endmodule
