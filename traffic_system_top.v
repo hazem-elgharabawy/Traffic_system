16ab8a7729bee2f1dd875819f1f41d4ed63e6a11
HazemElgharabawy <hazem.gharabawy6@gmail.com>
2024-11-28 16:08:26 +0200

    init files

---
+++ traffic_system_top.v
@@ -0,0 +1,14 @@
+module traffic_system_top (
+    input clk,
+    input rst_n,
+    input traffic_jam_0;
+    input traffic_jam_1;
+    input traffic_jam_2;
+    input traffic_jam_3;
+    output reg allow_0;
+    output reg allow_1;
+    output reg allow_2;
+    output reg allow_3;
+);
+    
+endmodule
