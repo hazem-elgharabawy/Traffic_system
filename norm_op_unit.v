ba745c1e9cecaea2a48d243fdf29cc84f970f2fb
HazemElgharabawy <hazem.gharabawy6@gmail.com>
2024-11-29 13:27:42 +0200

    corrected errors normal op works

--- norm_op_unit.v
+++ norm_op_unit.v
@@ -1,5 +1,6 @@
 module norm_op_unit (
-    input [1:0] traffic_light,
+    input norm_op_en,
+    input [1:0] current_road_norm,
     output reg  allow_0_norm,
     output reg  allow_1_norm,
     output reg  allow_2_norm,
@@ -7,38 +8,45 @@ module norm_op_unit (
 );
     
     always @(*) begin
-
-        case (traffic_light)
-            0 : begin
-                allow_0_norm = 1;
+        if (norm_op_en) begin
+            case (current_road_norm)
+                0 : begin
+                    allow_0_norm = 1;
+                    allow_1_norm = 0;
+                    allow_2_norm = 0;
+                    allow_3_norm = 0;
+                end
+                1 : begin
+                    allow_0_norm = 0;
+                    allow_1_norm = 1;
+                    allow_2_norm = 0;
+                    allow_3_norm = 0;
+                end
+                2 : begin
+                    allow_0_norm = 0;
+                    allow_1_norm = 0;
+                    allow_2_norm = 1;
+                    allow_3_norm = 0;
+                end
+                3 : begin
+                    allow_0_norm = 0;
+                    allow_1_norm = 0;
+                    allow_2_norm = 0;
+                    allow_3_norm = 1;
+                end 
+                default: begin
+                    allow_0_norm = 0;
+                    allow_1_norm = 0;
+                    allow_2_norm = 0;
+                    allow_3_norm = 0;
+                end
+            endcase
+        end
+        else begin
+                allow_0_norm = 0;
                 allow_1_norm = 0;
                 allow_2_norm = 0;
                 allow_3_norm = 0;
-            end
-            1 : begin
-                allow_0_norm = 0;
-                allow_1_norm = 1;
-                allow_2_norm = 0;
-                allow_3_norm = 0;
-            end
-            2 : begin
-                allow_0_norm = 0;
-                allow_1_norm = 0;
-                allow_2_norm = 1;
-                allow_3_norm = 0;
-            end
-            3 : begin
-                allow_0_norm = 0;
-                allow_1_norm = 0;
-                allow_2_norm = 0;
-                allow_3_norm = 1;
-            end 
-            default: begin
-                allow_0_norm = 0;
-                allow_1_norm = 0;
-                allow_2_norm = 0;
-                allow_3_norm = 0;
-            end
-        endcase
+        end
     end
 endmodule
--- traffic_system_top.v
+++ traffic_system_top.v
@@ -1,20 +1,20 @@
 module traffic_system_top (
-    input clk,
-    input rst_n,
-    input jam_sensor_0,
-    input jam_sensor_1,
-    input jam_sensor_2,
-    input jam_sensor_3,
-    output reg allow_0,
-    output reg allow_1,
-    output reg allow_2,
-    output reg allow_3
+    input   clk,
+    input   rst_n,
+    input   jam_sensor_0,
+    input   jam_sensor_1,
+    input   jam_sensor_2,
+    input   jam_sensor_3,
+    output  allow_0,
+    output  allow_1,
+    output  allow_2,
+    output  allow_3
 );
     
     wire allow_0_norm, allow_1_norm, allow_2_norm, allow_3_norm;
     wire allow_0_jam, allow_1_jam, allow_2_jam, allow_3_jam;
     wire norm_op_en, jam_op_en, norm_counter_en, jam_counter_en;
-    wire current_road_norm, current_road_jam;
+    wire [1:0] current_road_norm, current_road_jam;
     wire jam_start, jam_rotation;
 
     ctrl_unit control (
@@ -48,6 +48,7 @@ module traffic_system_top (
         );
 
     norm_op_unit normal_operation (
+        .norm_op_en(norm_op_en),
         .current_road_norm(current_road_norm),
         .allow_0_norm(allow_0_norm),
         .allow_1_norm(allow_1_norm),
